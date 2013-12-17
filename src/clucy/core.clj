850346428eb8df6152971dab479b2e7992d50270
Maximilian Karasz <maximilian.karasz@googlemail.com>
2013-12-17 10:20:40 +0100

    some debug printlns, search-and-delete numeric.
    just a sketch, would need to be fleshed out, dispatch on
    schema type-hints.

--- src/clucy/core.clj
+++ src/clucy/core.clj
@@ -386,6 +386,11 @@ fragments."
 					            :_max-score (.getMaxScore hits)})))))))))
 
 
+(defn search-and-delete-numeric [index field from to]
+  (with-open [writer (index-writer index)]
+    (let [query (NumericRangeQuery/newLongRange (as-str field) from to false false)]
+      (println query)
+      (.deleteDocuments writer query))))
 
 (defn search-and-delete
   "Search the supplied index with a query string and then delete all
@@ -398,10 +403,12 @@ of the results."
        ))
   ([index query default-field]
   (binding [*schema-hints* (or (meta index) *schema-hints*)] 
+    (println *schema-hints*)
     (binding [*doc-with-meta?* (and *doc-with-meta?*  (-> :*doc-with-meta?*  *schema-hints* false? not))
               *content* (and *content* (-> :*content*  *schema-hints* false? not))]
            (with-open [writer (index-writer index)]
 				       (let [parser (QueryParser. *version* (as-str default-field) *analyzer*)
 				             query  (.parse parser query)]
+                                         (println query)
 				         (.deleteDocuments writer query)))))))
 
